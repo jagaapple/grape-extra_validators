bda1842645a2d5ecd030d291034110ae80129024
Jaga Apple <jagaapple@uniboar.com>
2020-01-09 18:15:30 +0900

    [Update] Bump up version number to 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 # Changelog
+## 1.0.0 (2020-01-09)
+- Initial public release - [@jagaapple](https://github.com/jagaapple)
+
 ## 0.1.0 (2020-01-08)
 - Initial private release - [@jagaapple](https://github.com/jagaapple)
--- spec/grape/extra_validators/version_spec.rb
+++ spec/grape/extra_validators/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Grape::ExtraValidators, type: :feature do
-  example "The version number is 0.1.0" do
-    expect(described_class::VERSION).to eq("0.1.0")
+  example "The version number is 1.0.0" do
+    expect(described_class::VERSION).to eq("1.0.0")
   end
 end

669db5e77afca9c99aac6ed947753b09d8552e3f
Jaga Apple <jagaapple@uniboar.com>
2020-09-11 11:53:06 +0900

    [Add] Add support for giving Proc object to `minimum_value`

--- lib/grape/extra_validators/minimum_value.rb
+++ lib/grape/extra_validators/minimum_value.rb
@@ -14,10 +14,13 @@ module Grape
       def validate_param!(attr_name, params)
         return if !@required && params[attr_name].blank?
 
-        value = params[attr_name].to_i
-        return if value >= @option
+        minimum_value = @option.instance_of?(Proc) ? @option.call(params) : @option
+        return if minimum_value.blank?
 
-        message = "must be equal to or above #{@option}"
+        value = params[attr_name].to_i
+        return if value >= minimum_value
+
+        message = "must be equal to or above #{minimum_value}"
 
         fail Grape::Exceptions::Validation.new(params: [@scope.full_name(attr_name)], message: message)
       end
